91aa8449cfc9053710fec471312a32675ce6f51b
Rían Walter <72074324+Rian873@users.noreply.github.com>
2022-10-09 10:31:45 +0100

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,8 +1,11 @@
+# Function to multiply two numbers
 def multiply(n1,n2):
     return n1*n2
 
+# Function to add two numbers
 def add(n1,n2):
     return n1+n2
 
+# Function to subtract two numbers
 def subtract(n1,n2):
     return n1-n2
