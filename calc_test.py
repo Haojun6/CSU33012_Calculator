b3cd02510cec337f1091396afa0c40ab5b1c97d5
Rían Walter <72074324+Rian873@users.noreply.github.com>
2022-10-10 15:10:15 +0100

    Update calc_test.py

--- calc_test.py
+++ calc_test.py
@@ -2,16 +2,16 @@ from Calc import main
 
 # Testing functionality of addition, subtraction
 def test_addsub():
- assert main("9-10+2") == 1
+ assert main(9-10+2) == 1
 
 # Testing functionality of multiplaction, addition
 def test_muladd():
- assert main("10*2+5") == 25
+ assert main(10*2+5) == 25
 
 # Testing functionality of multiplication, subtraction
 def test_mulsub():
- assert main("18-2*5") == 8
+ assert main(18-2*5) == 8
 
 # Testing functionality of all operators
 def test_all():
- assert main("20-25+3*4") == 7
+ assert main(20-25+3*4) == 7
